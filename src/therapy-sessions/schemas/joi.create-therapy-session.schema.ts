42a3fba07b4d00eb5098e3f31ae7d54138f0ce0d
Vladislav Birilov <viap@inbox.ru>
2023-08-25 17:03:55 +0400

    Fixed validation error with date on create therapy session

--- src/therapy-sessions/schemas/joi.create-therapy-session.schema.ts
+++ src/therapy-sessions/schemas/joi.create-therapy-session.schema.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 import { joiPriceSchema } from 'src/common/schemas/joi.price.schema';
 
 export const joiCreateTherapySessionSchema = Joi.object({
-  date: Joi.date().required(),
+  date: Joi.string().required(),
   client: Joi.string().required(),
   psychologist: Joi.string().required(),
   duration: Joi.number().required(),
