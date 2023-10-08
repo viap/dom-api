08dde22e790b43d5f339ba2930c7a988e2746907
Vladislav Birilov <viap@inbox.ru>
2023-10-08 23:33:50 +0400

    Added env file with secrets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApiClientsModule } from './api-clients/api-clients.module';
@@ -6,21 +7,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
+import { NotificationsModule } from './notifications/notifications.module';
 import { PsychologistsModule } from './psychologists/psychologists.module';
 import { RolesGuard } from './roles/roles.guard';
 import { RolesModule } from './roles/roles.module';
-import { UsersModule } from './users/users.module';
-import { TherapySessionsModule } from './therapy-sessions/therapy-sessions.module';
 import { TherapyRequestsModule } from './therapy-requests/therapy-requests.module';
-import { EventsModule } from './events/events.module';
-import { NotificationsModule } from './notifications/notifications.module';
+import { TherapySessionsModule } from './therapy-sessions/therapy-sessions.module';
+import { UsersModule } from './users/users.module';
+import { cwd } from 'process';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'domData',
-      user: 'root',
-      pass: 'example',
+    ConfigModule.forRoot({
+      envFilePath: cwd() + '/config/.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: process.env.MONGO_DBNAME,
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
     }),
     UsersModule,
     AuthModule,
