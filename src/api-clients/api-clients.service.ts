4749ad38fcb7d37737ae103e79777bfe26d65dc5
Vladislav Birilov <viap@inbox.ru>
2023-10-12 16:27:55 +0400

    Fix apiClients loading from env file

--- src/api-clients/api-clients.service.ts
+++ src/api-clients/api-clients.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { clientConstants } from './consts';
 import { ApiClientDto } from './dto/api-client.dto';
 
 @Injectable()
 export class ApiClientsService {
-  private readonly clients: Array<ApiClientDto> = clientConstants.apiClients;
+  private readonly clients: Array<ApiClientDto> = [
+    {
+      name: process.env.BOT_CLIENT_NAME,
+      password: process.env.BOT_CLIENT_PASSWORD,
+    },
+  ];
   async findOne(name: string): Promise<ApiClientDto | undefined> {
     return this.clients.find((client) => client.name === name);
   }
--- src/api-clients/consts.ts
+++
@@ -1,8 +0,0 @@
-export const clientConstants = {
-  apiClients: [
-    {
-      name: process.env.BOT_CLIENT_NAME,
-      password: process.env.BOT_CLIENT_PASSWORD,
-    },
-  ],
-};
