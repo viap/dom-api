8d3d0a5e88a349ea9253d66ea1ded6a856c3a8d0
Vladislav Birilov <viap@inbox.ru>
2024-02-18 22:31:29 +0400

    Increase therapySessions deletion available time

--- src/therapy-sessions/therapy-sessions.service.ts
+++ src/therapy-sessions/therapy-sessions.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Price } from 'src/common/schemas/price.schema';
-import { getSumsForPrices } from 'src/common/utils/get-sums-for-prices';
 import groupBy from 'src/common/utils/group-by';
+import { Currencies } from 'src/psychologists/enums/currencies.enum';
 import { PsychologistsService } from 'src/psychologists/psychologists.service';
 import { PsychologistDocument } from 'src/psychologists/schemas/psychologist.schema';
 import { UsersService } from 'src/users/users.service';
 import { CreateTherapySessionDto } from './dto/create-therapy-session.dto';
+import { TherapySessionsControllerStatistic } from './dto/therapy-sessions-statistic.dto';
 import { UpdateTherapySessionDto } from './dto/update-therapy-session.dto';
 import {
   TherapySession,
   TherapySessionDocument,
 } from './schemas/therapy-session.schema';
-import { Currencies } from 'src/psychologists/enums/currencies.enum';
-import { TherapySessionsControllerStatistic } from './dto/therapy-sessions-statistic.dto';
 // import { from as fromArr, groupBy, mergeMap, toArray } from 'rxjs';
 
 const submodels = [
@@ -198,7 +197,7 @@ export class TherapySessionsService {
 
   async remove(id: string): Promise<boolean> {
     const therapySession = await this.getById(id);
-    if (therapySession && Date.now() - therapySession.timestamp < oneDay) {
+    if (therapySession && Date.now() - therapySession.timestamp < oneDay * 7) {
       return !!(await therapySession.deleteOne());
     }
 
