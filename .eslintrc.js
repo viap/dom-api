e0c1dd00587f6bf3470d6304228d425e690d874d
Vladislav Birilov <viap@inbox.ru>
2025-08-15 16:52:30 +0400

    Make changes for improve project performance

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'tsconfig.dev.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
